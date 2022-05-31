be10bce9207254b76e4d593fff0cce26774cc164
MahomedM <mahomedmachel@gmail.com>
2022-05-31 19:16:30 +0200

    Boolean OK

--- src/app/admin/admin-filter/admin-filter.component.ts
+++ src/app/admin/admin-filter/admin-filter.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Station } from 'src/app/models/station';
+import { StationService } from 'src/app/services/station.service';
 
 @Component({
   selector: 'app-admin-filter',
@@ -7,29 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminFilterComponent implements OnInit {
   searchText: any;
-  stations = [{ id: 1, name: "Olympiades", lift: "", escalator: "", callterminal: "", porte: "", line: "" },
-  { id: 2, name: "Bibliothèque F.M", lift: "", escalator: "", callterminal: " " },
-  { id: 3, name: "Cour Saint-Emillion", lift: "", escalator: "", callterminal: "" },
-  { id: 4, name: "Bibliothèques Francois Mitterand", lift: "", escalator: "", callterminal: "" },
-  { id: 5, name: "Bercy", lift: "", escalator: "", callterminal: "" },
-  { id: 6, name: "Chatêlet", lift: "", escalator: "", callterminal: "" },
-  { id: 7, name: "Pyramides", lift: "", escalator: "", callterminal: "" },
-  { id: 8, name: "Madeleine", lift: "", escalator: "", callterminal: "" },
-  { id: 9, name: "Saint-Lazare", lift: "", escalator: "", callterminal: " " },
-  { id: 10, name: "Poncardinet", lift: "", escalator: "", callterminal: "" },
-  { id: 11, name: "Porte de Clichy", lift: "", escalator: "", callterminal: "" },
-  { id: 12, name: "Saint-Ouen", lift: "", escalator: "", callterminal: "" },
-  { id: 13, name: "Mairie de Saint Ouen", lift: "", escalator: "", callterminal: "" },
-  { id: 14, name: "Chaussée d'An", lift: "", escalator: "", callterminal: "" },
-  ];
-  searchText2: any;
-  stations1 = [{ id: 15, name: 'Miromesnil', lift: "", escalator: "", callTerminal: "", porte: "", line: "" },
-  ];
 
-  constructor() { }
+  public listStations!: Station[];
+
+  constructor(private stationService: StationService) { }
 
   ngOnInit(): void {
-    console.log("");
+    this.stationService.getAllStations().subscribe((resp) => {
+      console.log(resp);
+      this.listStations = resp;
+    })
+  }
+  onClickDeleteStation(stationId: string | undefined) {
+    console.log(stationId);
+    if (stationId) {
+      this.stationService.deleteStation(stationId).subscribe({
+        next: (resp) => {
+          console.log(resp);
+          // Todo 
+          // Rechercher dans le tableau this.listStation l'objet station avec l'_id == stationId
+          // et le supprimer pour que notre liste de station n'affiche plus la station supprimée
+          //Pour ne pas à recharger la page pour voir la station supprimée
+          this.listStations = this.listStations.filter(station => station.id !== stationId);
+        },
+        error: (err) => { console.error(err) }
+      })
+    }
   }
 
 }
