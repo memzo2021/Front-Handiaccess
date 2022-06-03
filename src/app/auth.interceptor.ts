c5fe089654cb470f9d47f4527babd2193fe9b8ce
MahomedM <mahomedmachel@gmail.com>
2022-06-03 10:40:05 +0200

    sign-in sans role

--- src/app/auth.interceptor.ts
+++ src/app/auth.interceptor.ts
@@ -11,35 +11,21 @@ import jwtDecode from 'jwt-decode';
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-  constructor() {}
+  constructor() { }
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     console.log('coucou c\'est moi ');
     const token = localStorage.getItem('token');
-
-    if (token !== '' && token !== null) {
-       // je viens modifier la requete sortante pour 
-      // y ajouter le token trouvé dans le localStorage
-
-      // vérif date expiration
-      const decodedToken: { sub: string, iat: number, exp: number } = jwtDecode(token);
-      const expirationDate = new Date(decodedToken.exp * 1000);
-      const currentDate = new Date();
-
-      if (expirationDate < currentDate) { 
-        return next.handle(request);
-      }
+    if (token !== null && token !== '') {
 
       const authReq = request.clone(
         {
-       //   headers: request.headers.set('Authorization', `Bearer ${token}`)
-            setHeaders:{Authorization: `Bearer ${token}`}
+          setHeaders: { 'Authorization': `Bearer ${token}` } // version plus courte
         }
       )
-      // je laisse passer (vers le back) la requête possédant un token 
+
       return next.handle(authReq);
     } else {
-      // si je n'ai pas de token, je ne modifie la requete sortante et je la laisse passer
       return next.handle(request);
     }
 
--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -15,33 +15,33 @@ export class AuthGuard implements CanActivate {
     console.log('Passage par le guard !!!');
 
     const token = localStorage.getItem('token');
-    
-    
-    if (token !== null) {
-      const decodedToken: { sub: string, iat: number, exp: number } = jwtDecode(token);
+
+    if (token !== null && token !== '') {
+      const decodedToken: { userId: string, iat: number, exp: number } = jwtDecode(token);
+
+      console.log(decodedToken);
       const expirationDate = new Date(decodedToken.exp * 1000);
+
       const currentDate = new Date();
-        //const currentDate = new Date("05/29/22"); // juste pour le test
+
 
       if (expirationDate < currentDate) {
-        // token périmé
-        // 1 - Supprimer mon token du localstorage
-        // 2 - Rediriger mon utilisateur vers /sign-in
-        // 3 - retourner false
+
         localStorage.removeItem('token');
-        this.router.navigateByUrl('/sign-in');
+        this.router.navigateByUrl('/app-sign-in');
         return false;
 
       } else {
-        // token valide (au niveau de sa date)
-        // 1 - retourner true
         return true;
       }
 
     } else {
-      this.router.navigateByUrl('/sign-in');
+      this.router.navigateByUrl('/app-sign-in');
       return false;
     }
   }
 
 }
+
+
+
